d07bbcda9d953a4f12904fa49df1d31cf2f2b1cc
navyakhurana <64686397+navyakhurana@users.noreply.github.com>
2020-07-20 14:21:18 +0530

    Add files via upload

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameManager1 gameManager1;
+    
+    void OnTriggerEnter ()
+    {
+        gameManager1.CompleteLevel();
+    }
+}
---
+++ Assets/coin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 0f, 100f) * Time.deltaTime);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
